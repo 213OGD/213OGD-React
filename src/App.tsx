91f65c96013aee6c76d8e2234da8a98d6ab08412
S2LF <sdeuhelf@gmail.com>
2020-12-17 14:29:27 +0100

    add .env & FormEvent Type

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,44 @@ import './App.css';
 function App(): JSX.Element {
   const [files, setFiles] = useState<DatasProps[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await Axios('http://localhost:5000/api/file/list');
-        // eslint-disable-next-line no-console
-        console.log(result.data.result);
-        setFiles(result.data.result);
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const result = await Axios('http://localhost:5000/api/file/list');
+  //       // eslint-disable-next-line no-console
+  //       console.log(result.data.result);
+  //       setFiles(result.data.result);
+  //     } catch (error) {
+  //       // eslint-disable-next-line no-console
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
+  const datas = [
+    {
+      id: '1',
+      name: 'Backlog du POC',
+      webViewLink:
+        'https://docs.google.com/spreadsheets/d/1GBkzbQEGAU7lONfR_AQDPTde4-GodcfT_Xqb2-V6sUg/edit?usp=drivesdk',
+      iconLink:
+        'https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.spreadsheet',
+      tags: ['SCRUM', 'react', 'node'],
+    },
+    {
+      id: '2',
+      name: 'Test',
+      webViewLink:
+        'https://docs.google.com/spreadsheets/d/1GBkzbQEGAU7lONfR_AQDPTde4-GodcfT_Xqb2-V6sUg/edit?usp=drivesdk',
+      iconLink:
+        'https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.spreadsheet',
+      tags: ['SCRUM', 'react', 'node'],
+    },
+  ];
+
+  console.log(process.env.REACT_APP_URI);
+  console.log(process.env.REACT_APP_CREDZ);
 
   return (
     <div className="container">
@@ -27,7 +51,7 @@ function App(): JSX.Element {
         <h1>213 Odyssey Google Drive</h1>
       </header>
       <div className="container-cards">
-        {files.map((data) => (
+        {datas.map((data) => (
           // eslint-disable-next-line react/jsx-props-no-spreading
           <CardFile key={data.id} {...data} />
         ))}
