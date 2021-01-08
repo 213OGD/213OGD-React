158e4edc032d2204c296527c57a7cc70f20261e1
s2lf <sdeuhelf@gmail.com>
2021-01-08 14:39:46 +0100

    update package.json & rm useless code

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,6 @@ import './App.css';
 import SideBar from './components/SideBar';
 
 function App(): JSX.Element {
-  const [files, setFiles] = useState<DatasProps[]>([]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const result = await Axios('http://localhost:5000/api/file/list');
-  //       // eslint-disable-next-line no-console
-  //       console.log(result.data.result);
-  //       setFiles(result.data.result);
-  //     } catch (error) {
-  //       // eslint-disable-next-line no-console
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
-  // const datas = [
-  //   {
-  //     id: '1',
-  //     name: 'Backlog du POC',
-  //     webViewLink:
-  //       'https://docs.google.com/spreadsheets/d/1GBkzbQEGAU7lONfR_AQDPTde4-GodcfT_Xqb2-V6sUg/edit?usp=drivesdk',
-  //     iconLink:
-  //       'https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.spreadsheet',
-  //     tags: ['SCRUM', 'react', 'node'],
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Test',
-  //     webViewLink:
-  //       'https://docs.google.com/spreadsheets/d/1GBkzbQEGAU7lONfR_AQDPTde4-GodcfT_Xqb2-V6sUg/edit?usp=drivesdk',
-  //     iconLink:
-  //       'https://drive-thirdparty.googleusercontent.com/16/type/application/vnd.google-apps.spreadsheet',
-  //     tags: ['SCRUM', 'react', 'node'],
-  //   },
-  // ];
-
   const { loading, error, data } = useQuery(GET_FILES);
 
   // console.log(process.env.REACT_APP_URI, 'FILES', data);
