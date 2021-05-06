3b69a4e5a67adfa83fed11ea86740d8343203f9c
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-05-06 15:07:29 +0200

    modif pour staging et prod

--- start_staging.sh
+++ start_staging.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git fetch origin && git reset --hard origin/master && git clean -f -d
+git fetch origin && git reset --hard origin/staging && git clean -f -d
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yml -f docker-compose-prod.yml up --build -d
